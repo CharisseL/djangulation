80c4ecbffa10bbb58f2e2ef607f964cd34bf7794
CharisseL <charisse8@gmail.com>
2016-09-08 15:37:31 -0500

    authentication app installed

--- authentication/models.py
+++ authentication/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import BaseUserManager
 
 class AccountManager(BaseUserManager):
     def create_user(self, email, password=None, **kwargs):
